45af385f650e92a05c2767e04c0069710cc8e40d
njshockey <shockeynj@gmail.com>
2023-05-23 19:22:36 -0400

    Fetch logs data to console

--- src/lib/components/ApiQuery.svelte
+++ src/lib/components/ApiQuery.svelte
@@ -1,4 +1,22 @@
-<script lang="ts"></script>
+<script lang="ts">
+let requestInput: String;
+
+function getRequest(url: String) {
+	fetch(url as RequestInfo)
+		.then(function (res) {
+			if (res.ok) {
+				return res.json();
+			}
+			throw new Error("Response was not OK.");
+		})
+		.then(function (data) {
+			console.log(data);
+		})
+		.catch(function (error) {
+			console.error("Error: " + error);
+		});
+}
+</script>
 
 <main>
 	<select class="select select-bordered w-full max-w-xs">
@@ -10,6 +28,10 @@
 		type="text"
 		placeholder="URL"
 		class="input input-bordered w-full max-w-xs"
+		bind:value={requestInput}
 	/>
-	<button class="btn btn-primary">Send</button>
+	<button
+		class="btn btn-primary"
+		on:click={() => getRequest(requestInput)}>Send</button
+	>
 </main>
