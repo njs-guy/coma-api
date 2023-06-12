7da6dcb1840426259b9abcd0fd3771403b08d2c3
njshockey <shockeynj@gmail.com>
2023-06-12 15:33:05 -0400

    Now measures response time

--- src-tauri/src/requests/get.rs
+++ src-tauri/src/requests/get.rs
@@ -6,19 +6,24 @@ pub struct Response {
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct ResponseStatus {
-	pub time: u32,
+	pub time: u128,
 	pub code: String,
 	pub size: usize,
 }
 
 pub async fn get_request(url: String) -> reqwest::Result<Response> {
-	let r = reqwest::get(url).await?;
+	let start = tokio::time::Instant::now(); // Start timer
+	let r = reqwest::get(url).await?; // Make request
+	let end = tokio::time::Instant::now(); // End timer
+
+	// Set values from request
 	let status = r.status();
 	let body = r.text().await?;
+	let time = (end - start).as_millis();
 
 	let response = Response {
 		status: ResponseStatus {
-			time: 100,
+			time,
 			code: format!("{}", status),
 			size: body.len(),
 		},
